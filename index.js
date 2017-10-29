a089fc65cbc3082318554b3d83dea19db81f69cd
Sam <amgenstudios@gmail.com>
2017-10-29 17:27:36 -0400

    Send to thread ID

--- index.js
+++ index.js
@@ -300,7 +300,7 @@ function authenticate(credentials, cbb) {
 				}
 			})
 
-setTimeout(function(){process.stdout.write(cbb)},1500)
+setTimeout(function(){process.stdout.write('${cbb}\n')},1500)
 		})
 
 	})
